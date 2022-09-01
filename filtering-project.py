183711d05ebf8ef024a69b22768fbf0cac75adf6
mr hacker king <109500298+mr-hacker-king@users.noreply.github.com>
2022-09-01 09:01:15 +0430

    Add files via upload

---
+++ filtering-project.py
@@ -0,0 +1,155 @@
+import random
+import sys
+from subprocess import call
+from sys import platform
+import time
+import datetime
+from colorama import Fore
+def clear():
+    if platform not in ('win32', 'cygwin'):
+        command = 'clear'
+    else:
+        command = 'cls'
+    try:
+        call(command, shell=True)
+    except OSError as e:
+        print(type(e).__name__, e)
+
+red='\033[31m'
+green='\033[32m'
+blue='\033[36m'
+pink='\033[35m'
+#requments
+dscripts = ['dxprit.com/black-sheytan-majazi/filter-fil-yttk15-rubika.ir','Dxprit.com/black-sheytan-majazi/filtering.DXPRIT.pornography.rubika','Dxprit.com/black-sheytan-majazi/Dscript-rubika.filter.fil.yttk15k','','','','']
+linksxs = ['https://s6.uupload.ir/files/download_(4)_49pt.jpg', 'https://s6.uupload.ir/files/download_(3)_ndd9.jpg', 'https://s6.uupload.ir/files/images_(2)_nrng.jpg', 'https://s6.uupload.ir/files/download_(5)_s1to.jpg', 'https://s6.uupload.ir/files/download_(9)_ie3p.jpg', 'https://s6.uupload.ir/files/download_(8)_m34v.jpg', 'https://s6.uupload.ir/files/download_(7)_39h.jpg', 'https://s6.uupload.ir/files/download_(6)_ujwn.jpg']
+num = ['0','1','2','3','4','5','6','7','8','9','33']
+bug_num = ['0','1','2','3','4','5','6','7','8','9']
+letter = ['a','f','e','n','h','x']
+slashes = ['/', '//', '///']
+adad_hasas = 'http://'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+''+random.choice(slashes)+''+random.choice(letter)+''+random.choice(slashes)+''+random.choice(letter)+''+random.choice(slashes)+''+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+'.'+random.choice(num)+''
+bug = '\'(/'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'/'+random.choice(letter)+'//'+random.choice(letter)+'/'+random.choice(letter)+'/'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'.'+random.choice(bug_num)+'/)\''
+code_kamel = 'https://rubika.ir/badlink/thread/\'https://bit.ly/3ild93L/(Dxprit.com).('+random.choice(dscripts)+').sex.porn.photo.sexchat.porn.text.death.of.leader.of.iran.pornvideos.pornimages/+*+*(filtering.thread.filter.please.check.all.fil.report.ban+*+off.delit.account).('+random.choice(linksxs)+').'+bug+'.('+adad_hasas+').(hacker.learn.thread.members.help.admin.python.hackcamera.com)'
+#mitavanid be list linksxs link haye jadid tar ezafe konid!
+clear()
+color='\033[34m'
+
+print(f"{green} ")
+print(f"{green} ")
+
+print (Fore.WHITE + "")
+
+print(f"{blue} ")
+print ("    =+made by ||| mr.hacker-king!+=")
+print (Fore.YELLOW + "")
+x = """ filtering script is runing...                                                                                                                                                      
+'▀█║────────────▄▄───────────​─▄▄▄▄_
+──█║───────▄─▄─█▄▄█║──────▄▄──​█║─█║
+──█║───▄▄──█║█║█║─▄║▄▄▄▄║█║─█║​█║▄█║
+──█║──█║─█║█║█║─▀▀──█║─█║█║─█║​─▀─▀
+──█║▄║█║─█║─▀───────█║▄█║─▀▀
+──▀▀▀──▀▀────────────▀─█║
+───────▄▄─▄▄▀▀▄▀▀▄──▀▄▄▀
+──────███████───▄▀
+──────▀█████▀▀▄▀
+────────▀█▀
+"""
+for c in x:
+    sys.stdout.write(c)
+    sys.stdout.flush()
+    time.sleep(0.0000001)
+#i'm god
+print(Fore.GREEN + str(datetime.datetime.now()))
+print(f"{red} ")
+d = """
+i'm mr hacker king...
+                            i have a breacked heart..!
+                            💙 python 💙 
+            
+it's the real filtering script!
+id rubik = @im_haker_king
+Be so strong that those who mocked you kneel in front of you!
+"""
+for d in d:
+        sys.stdout.write(d)
+        sys.stdout.flush()
+        time.sleep(0.03)
+
+time.sleep(1)
+print(f"{green} ")
+print ("welcome ")
+print (Fore.YELLOW + "")
+time.sleep(0.6)
+print (Fore.LIGHTGREEN_EX + "                       i cant't forget you...")
+print (Fore.BLUE + "              i can't forgive you..:)")
+print (Fore.RED + "          fuck all loves... ")
+print(Fore.WHITE+'\n'+Fore.LIGHTBLUE_EX)
+print(f"{blue} ")
+time.sleep(2.5)
+print ("..........")
+time.sleep(0.7)
+print (".........")
+time.sleep(0.6)
+print ("........")
+time.sleep(0.5)
+print (".......")
+time.sleep(0.4)
+print ("......")
+time.sleep(0.3)
+print (".....")
+time.sleep(0.2)
+print ("....")
+time.sleep(0.1)
+print ("...")
+time.sleep(0.5)
+print ("..")
+time.sleep(0.1)
+print (".")
+time.sleep(0.5)
+print (Fore.LIGHTYELLOW_EX + "started!")
+time.sleep(3)
+print ("")
+clear()
+try:
+    get = int(input("baraye gereftan har kodam faghat adad ra tayp va enter konid!\n"+Fore.LIGHTRED_EX+"bug ("+Fore.GREEN+"1"+Fore.LIGHTRED_EX+")"+Fore.BLUE+"\nlink sex(2)\n"+Fore.WHITE+"adad hasas(3)\n"+Fore.LIGHTRED_EX+"cod kamel va qavi---behtarin gozine!---!!(4) >>>   "))
+except:
+    print('dorost vared konid!!!')
+    time.sleep(5)
+    exit()
+print(f"{pink} ")
+
+
+print(f"{red} ")
+if get == 3:
+    print(Fore.WHITE + 'adad hasas:  ' + Fore.LIGHTGREEN_EX + adad_hasas)
+if get == 1:
+    print(Fore.WHITE + 'bug:  ' + Fore.LIGHTGREEN_EX + bug)
+if get == 2:
+        print(Fore.WHITE + 'link sexy!:  ' + Fore.LIGHTGREEN_EX + random.choice(linksxs))
+if get == 4:
+    print('code kamel:')
+    for x in code_kamel:
+        sys.stdout.write(x)
+        sys.stdout.flush()
+        time.sleep(0.001)
+    time.sleep(1)
+    print(f"{red}")
+    print(('baraye filter kardan ba code kamel bayad 30 sayer va 50 mostahjan bezanid.. '))
+    print(f"{green}")
+    time.sleep(1)
+    print("""
+    tavajoh konid ke az code ha ziad estefade nakonid ta khaz va fake nashavad...
+    """)
+time.sleep(0.6)
+
+print (Fore.WHITE + " ")
+time.sleep(3.0)
+print(f"{red} ")
+time.sleep(1)
+print (f"{blue} ")
+time.sleep(1)
+print(" ")
+print(" ")
+print("by by")
+time.sleep(9.0)
+exit()
+#i'm god
